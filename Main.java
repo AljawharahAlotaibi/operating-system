7bf66ac5b632936b4527bf906fc0952b6c52cdcd
afnanAlonazi <146772610+afnanAlonazi@users.noreply.github.com>
2024-03-30 01:47:20 +0300

    add menu Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+
+    //	static PCB[] Q1 ;
+//	static PCB[] Q2 ;
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,6 +16,30 @@ public class Main {
         PCB[] Q1 = new PCB[numProcesses];
         PCB[] Q2 = new PCB[numProcesses];
 
-    
-    }
+         int answer = 0;
+         do {
+ 			System.out.println("Chosse form the following menu:");
+ 			System.out.println("1) Enter process’ information. ");
+ 			System.out.println("2) Report detailed information about each process and different scheduling criteria. ");
+ 			System.out.println("3) Exit the program.");
+ 
+ 			answer = scanner.nextInt();
+
+ 			switch (answer) {
+ 		
+ 			case 1:
+ 				
+ 				break;
+ 			case 2:
+ 				
+ 			case 3:
+ 				System.out.println("Exiting.. ");
+ 				break;
+
+ 			default:
+ 				System.out.println("Invalid input, try again");
+ 			}
+ 		} while (answer != 3);
+ 	}
+
 }
