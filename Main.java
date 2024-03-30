3d498bb141b64fe78efbfb0740f34d7ede15ff4a
afnanAlonazi <146772610+afnanAlonazi@users.noreply.github.com>
2024-03-30 04:41:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Main {
 
-    //	static PCB[] Q1 ;
-//	static PCB[] Q2 ;
+static PCB[] Q1 ;
+static PCB[] Q2 ;
     
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         // Prompt the user to enter the number of processes (P)
@@ -13,8 +15,8 @@ public class Main {
         int numProcesses = scanner.nextInt();
 
         // Create arrays to represent Q1 and Q2
-        PCB[] Q1 = new PCB[numProcesses];
-        PCB[] Q2 = new PCB[numProcesses];
+         Q1 = new PCB[numProcesses];
+         Q2 = new PCB[numProcesses];
 
          int answer = 0;
          do {
@@ -31,6 +33,11 @@ public class Main {
  				
  				break;
  			case 2:
+                 for (int i = 0; i < numProcesses; i++) {
+					System.out.printf("%-3d", i + 1);
+					Q1[i].StatusDetail();
+				}
+ 				WriteReport();
  				break;
  			case 3:
  				System.out.println("Exiting.. ");
@@ -41,5 +48,23 @@ public class Main {
  			}
  		} while (answer != 3);
  	}
-
+public static void WriteReport() throws IOException {
+ 
+			BufferedWriter outputfile = new BufferedWriter(new FileWriter("Report.txt"));
+			for (int i = 0; i < Q1.length; i++) {
+				outputfile.write(" | The processID " +Q1[i].getProcessID()  );
+				outputfile.write(" | The priority : " +Q1[i].getPriority());
+				outputfile.write(" | The arrivalTime : " + Q1[i].getArrivalTime());
+				outputfile.write(" | The cpuBurstTime :" + Q1[i].getCpuBurstTime());
+				outputfile.write(" | Starting time: " + Q1[i].getStartTime() );
+				outputfile.write(" | termination time  " + Q1[i].getTerminationTime() );
+				outputfile.write(" | turnaround time  " + Q1[i].getTurnaroundTime());
+				outputfile.write(" | waiting time  " + Q1[i].getWaitingTime() );
+				outputfile.write(" | response time  " + Q1[i].getResponseTime() );
+				
+				outputfile.write(" ");
+			}
+			//outputfile.flush();
+			outputfile.close();
+		}
 }
