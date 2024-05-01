280b8511066e471aed93614397ff93b28728c237
AljawharahAlotaibi <145213331+AljawharahAlotaibi@users.noreply.github.com>
2024-05-01 17:48:36 +0300

    submission

--- Main.java
+++ Main.java
@@ -6,19 +6,24 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
+
+
+
 public class Main {
-
-    static PCB[] Q1  = new PCB[100];
+	static PCB[] Q1  = new PCB[100];
 	static PCB[] Q2 = new PCB[100];
-	static PCB[] completedProcess=new PCB[100];
-	static int completed =0;
+	static PCB[] completedProcess=new PCB[100]; //after deleting process we save it here to printing it 
+	static int completed =0; // counter for completedProcess array
 	static  int currentTime = 0;
-	static StringBuilder schedulingOrder = new StringBuilder("[");
-    
-public static void main(String[] args)  throws IOException {
-    Scanner scanner = new Scanner(System.in);
-
-   
+	static String  processOrder="[";
+	static double avgTurnaroundTime=0;
+	static double avgWaitingTime = 0;
+	static double avgResponseTime = 0;
+	
+	public static void main(String[] args)  throws IOException {
+	Scanner scanner = new Scanner(System.in);
+	 
+	      
 	    int answer = 0;
         do {
         System.out.println("Chosse form the following menu:");
@@ -36,7 +41,7 @@ public static void main(String[] args)  throws IOException {
         	 int  numProcesses=0;
     	    do {
     	        System.out.print("Enter the number of processes (P): ");
-    	        while (!scanner.hasNextInt()){
+    	        while (!scanner.hasNextInt()){ //hasNextInt() is used to check if the next token in the input stream is an integer, while nextInt() is used to actually read that integer from the input stream.
     	            System.out.println("Invalid input. Please enter a positive integer.");
     	            scanner.next(); // Consume the invalid input
     	        }//End of while
@@ -49,7 +54,8 @@ public static void main(String[] args)  throws IOException {
     	    
   	      System.out.println("1) Enter process’ information. ");
   	      for (int  i= 0; i < numProcesses; i++){
-                String pid = "P" + (i + 1);
+  	    	    int numberOfprocess=i+1;
+                String pid = "P" + (i + 1); // to help us to sort the preccess upon the process ID to print it
                 System.out.println("Enter "+pid+" information:");
                 System.out.println("Enter priority for " + pid + " (1 or 2): ");
                 int priority = scanner.nextInt();
@@ -61,12 +67,15 @@ public static void main(String[] args)  throws IOException {
                 int arrivalTime = scanner.nextInt();
                 System.out.print("Enter CPU burst for " + pid + ": ");
                 int cpuBurst = scanner.nextInt();
-                PCB process = new PCB(pid, priority, arrivalTime, cpuBurst);
+                PCB process = new PCB(numberOfprocess,pid, priority, arrivalTime, cpuBurst);
                 addProcess(process);
                 }
     	    
   	      scheduleProcesses();
   	      excuteQ2() ;
+  	      calculateAverages();
+  	     // System.out.println( processOrder +"]");
+  	    
   	      writeInFile();
             break;
         case 2:
@@ -110,48 +119,57 @@ public static void main(String[] args)  throws IOException {
 	      
 	      public static void scheduleProcesses() {
 
-	    	    Arrays.sort(Q1, Comparator.nullsLast(Comparator.comparingInt(PCB::getArrivalTime)));
-	    	  
+	    	    Arrays.sort(Q1, Comparator.nullsLast(Comparator.comparingInt(PCB::getArrivalTime))); //nullsLast to ignore the empty indexes in the array
+// there is 3 casses , 1-if the burstTime<=3  2- 	    	  
 	    	    int quantum = 3;
-	    	    StringBuilder schedulingOrder = new StringBuilder("[");
+	    	    PCB deleted=null;
+	    	    boolean isChanged = false;
 
 	    	    int i = 0;
 	    	    while (i < Q1.length && Q1[i] != null) {
 	    	        if (currentTime >= Q1[i].getArrivalTime()) {
 	    	            if (Q1[i].getRemainingTime() <= quantum) {
-	    	            	if(Q1[i].getStartTime()==-1){//process not started before
+	    	            	if(Q1[i].getStartTime()==-1){ //process not started before
 	    	            		Q1[i].setStartTime(currentTime);}	
 	    	                currentTime += Q1[i].getRemainingTime();
 	    	                Q1[i].setTerminationTime(currentTime);
-	    	                schedulingOrder.append(Q1[i].getProcessID()).append("|");  // Add the process to the scheduling order
-	    	                System.out.println(Q1[i].getProcessID());//for test
+	    	                processOrder= processOrder+Q1[i].getProcessID()+"|";
 	    	                Q1[i].setRemainingTime(0);
-	    	              if(completed<100)
-	    	                  completedProcess[completed++]=remove(Q1, i);// The process has been executed completely
-	    	        
+	    	              if(completed<100){
+	    	            	   deleted= remove(Q1, i); // we use deleted to help us to know if there is deleted or not
+	    	            	   completedProcess[completed++]=deleted;// The process has been executed completely
+                          }else{ // array is full
+                            break;
+                          }
+
 	    	                } //End of if2
 	    	            
 	    	            
 	    	            else {
+	    	            	deleted=null;
 	    	            	Q1[i].setRemainingTime(Q1[i].getRemainingTime() - quantum);
 	    	                if(Q1[i].getStartTime()==-1){//process not started before
 	    	                	Q1[i].setStartTime(currentTime);}
 	    	                currentTime += quantum;
-	    	                if (i + 1 < Q1.length && Q1[i + 1] != null && Q1[i + 1].getArrivalTime() <= currentTime) {
-	    	                    changeIndex(Q1, i, currentTime);
+	    	                processOrder= processOrder+Q1[i].getProcessID()+"|";  // Add the process to the scheduling order
+	    	                if (i + 1 < Q1.length && Q1[i + 1] != null && Q1[i + 1].getArrivalTime() <= currentTime) { // to search where to put the proccess in order to excute it by arrival time
+	    	                	 isChanged= changeIndex(Q1, i, currentTime);
 	    	                }
 	    	               
 	    	            }
 	    	            // Check if the loop should reset the index
-	    	            if(Q1.length!=i+1)
-	    	            if (Q1[i]!= null && Q1[i+ 1]== null )
+	    	            if(Q1.length!=i+1)	
+	    	            if (Q1[i]!= null && Q1[i+ 1]== null ) {
 	    	            	if (findFirstNotNullIndex(Q1)!=-1)
 	    	            	 i = findFirstNotNullIndex(Q1);
-	    	            else if(currentTime >= Q1[i].getArrivalTime()&&currentTime < Q1[i + 1].getArrivalTime())
+	    	            }
+	    	            else if(Q1[i]!= null&&currentTime >= Q1[i].getArrivalTime()&&currentTime < Q1[i + 1].getArrivalTime()) {
 	    	            	if (findFirstNotNullIndex(Q1)!=-1)
 		    	            	 i = findFirstNotNullIndex(Q1);
-	    	            else
-	    	            	i++;
+	    	            }
+	    	            	else if (deleted==null&&isChanged==false) // (if procces t) if we delete proccess we didn't want to incrment bc alredy we did shifting in delete
+	    	                	i++;
+	    	            	
 	    	            
 	    	           
 	    	        } else {
@@ -159,16 +177,19 @@ public static void main(String[] args)  throws IOException {
 	    	        }
 	    	    }
 
-//	    	    schedulingOrder.append("]");
-//	    	    return "Scheduling Order: " + schedulingOrder.toString();
+//	    	    
+//	    	    
+	    	    
 	    	}
 	      public static int findFirstNotNullIndex(PCB[] array) {
+	    	  int index=-1;
 	          for (int i = 0; i < array.length; i++) {
 	              if (array[i] != null) {
-	                  return i;
+	            	  index=i;
+	            	  break;   
 	              }
 	          }	
-	          return -1;
+	          return index;
 	      }
 	      public static PCB remove(PCB[] array, int indexToRemove) {
 	    	   if (indexToRemove < 0 || indexToRemove >= array.length) {
@@ -191,21 +212,25 @@ public static void main(String[] args)  throws IOException {
 	    	    return deletedObj;
 	    	}
 	      
-	    	public static void changeIndex(PCB[] array, int index, int currentTime) {
+	    	public static boolean changeIndex(PCB[] array, int index, int currentTime) {
+	    		boolean isChanged=false;
 	    	    if (index < 0 || index >= array.length) {
 	    	        // Invalid index, do nothing
-	    	        return;
-	    	    }
+	    	       }
 	    	    
 	    	    PCB target = array[index];
 	    	    int firstGreaterIndex = -1;
 
 	    	    // Find the index of the first element with a greater arrival time
 	    	    for (int i = index + 1; i < array.length; i++) {
+	    	    	if(array[i]!=null)
 	    	        if (array[i].getArrivalTime() > currentTime) {
 	    	            firstGreaterIndex = i;
+	    	            isChanged=true;
 	    	            break;
 	    	        }
+	    	    	
+	    	    	
 	    	    }
 
 	    	    // If a greater index is found, shift elements to make space and insert the target
@@ -215,52 +240,62 @@ public static void main(String[] args)  throws IOException {
 	    	        }
 	    	        array[firstGreaterIndex - 1] = target;
 	    	    }
+	    	    return isChanged;
 	    	}
 	    	//////////////////////
 	    	 public static void excuteQ2() {
 	    		 
 	    		 // Sort Q2 based on CPU burst time SJF
-	    		 Arrays.sort(Q1, Comparator.nullsLast(Comparator.comparingInt(PCB::getCpuBurstTime)));
-	    	      
+	    		 Arrays.sort(Q2, Comparator.nullsLast(Comparator.comparingInt(PCB::getCpuBurstTime)));
+	    		 PCB deleted = null;
 
-        // Repeatedly iterate through Q2 until all processes are executed
-        while (true) {
-            boolean processExecuted = false; // Track if any process is executed in this iteration
-            for (int i = 0; i < Q2.length; i++) {
-                PCB process = Q2[i];
-                if (process != null && process.getArrivalTime() <= currentTime) {
-                    processExecuted = true; // A process is executed in this iteration
-                    excuteProcess(process); // Execute the process for its CPU burst time
-                    remove(Q2, i); 
-                    break;
-                }else {
-                    if(processArriveAtQ1()){ //check q1 if there is arrival excute it and don't twice increment time
-                        processExecuted = true; // A process is executed in this iteration
-                    }
-                }
-            }
-            if (empty(Q2) && empty(Q1)) {
-                // All processes executed, exit the loop
-                break;
-            }
-            if (!processExecuted) {
-                // No process executed, increment currentTime
-                currentTime++;
-            }
-        }
-        
-    }
-    public static void excuteProcess(PCB process){
-
-	    	        int cpuBurst = process.getCpuBurstTime();
-	    	        
-	    	        //for each ms of the time 
-	    	        for(int i=0;i<cpuBurst;i++){ 
-	    	        System.out.println("Running process " + process.getProcessID() + "  with priority 2");
-	    	        currentTime++;    
-	    	        process.setCpuBurstTime( process.getCpuBurstTime()-1 ); //could not be necessary !!
-	    	        processArriveAtQ1(); //check q1 if there is arrival, excute it                
+	    	        // Repeatedly iterate through Q2 until all processes are executed
+	    	        while (true) {
+	    	            boolean processExecuted = false; // Track if any process is executed in this iteration
+	    	            for (int i = 0; i < Q2.length; i++) {
+	    	                PCB process = Q2[i];
+	    	                if (process != null && process.getArrivalTime() <= currentTime) {
+	    	                    processExecuted = true; // A process is executed in this iteration
+	    	                    excuteProcess(process); // Execute the process for its CPU burst time
+	    	                    if(completed<100) { // if not full
+	 	    	            	   deleted= remove(Q2, i);
+	 	    	            	   completedProcess[completed++]=deleted;// The process has been executed completely
+	    	                   break;
+	    	                   }    
+	 	
+	    	                   
+	    	                }else {
+	    	                    if(processArriveAtQ1()){ //check q1 if there is arrival excute it and don't twice increment time
+	    	                        processExecuted = true; // A process is executed in this iteration
+	    	                    }
+	    	                }
+	    	            }
+	    	            if (empty(Q2) && empty(Q1)) {
+	    	                // All processes executed, exit the loop
+	    	                break;
+	    	            }
+	    	            if (!processExecuted) {
+	    	                // No process executed, increment currentTime
+	    	                currentTime++;
+	    	            }
 	    	        }
+	    	        
+	    	    }
+	    	    public static void excuteProcess(PCB process){
+	    	    	process.setStartTime(currentTime);
+	    	    	 boolean processArrive=false;
+	    	         int cpuBurst = process.getCpuBurstTime();
+	    	         processOrder= processOrder+process.getProcessID()+"|";
+	    	        //for each ms of the time 
+	    	        for(int i=0;i<cpuBurst;i++){
+	    	        if(processArrive==true)
+	    	        	processOrder= processOrder+process.getProcessID()+"|";
+	    	        currentTime++;    
+	    	        process.setRemainingTime(process.getRemainingTime()-1); //could not be necessary !!
+	    	        processArrive= processArriveAtQ1(); //check q1 if there is arrival, excute it 
+	    	        
+	    	        }
+	    	        process.setTerminationTime(currentTime);
 	    	    }
 
 	    	    public static boolean empty(PCB[] array) {
@@ -283,48 +318,104 @@ public static void main(String[] args)  throws IOException {
 	    	    } 
 
 
-	    	public static void writeInFile(){
-	    	
-	    		try {
-	    		    File f = new File("report1.txt");
-	    		    FileOutputStream f2 = new FileOutputStream(f);
-	    		    PrintWriter outputfile= new PrintWriter(f2);
-	    		    for (int i = 0; i < 100; i++) {
-	    		    	if(completedProcess[i]!=null) {
-	    		    		if(  completedProcess[i].getProcessID().equals("P" + (i + 1) )      )
-	    		    		outputfile.println(" The processID " +completedProcess[i].getProcessID()  );
-	    		            outputfile.println(" The priority : " +completedProcess[i].getPriority());
-	    		            outputfile.println(" The arrivalTime : " +completedProcess[i].getArrivalTime());
-	    		            outputfile.println(" The cpuBurstTime :" + completedProcess[i].getCpuBurstTime());
-	    		            outputfile.println(" Starting time: " + completedProcess[i].getStartTime() );
-	    		            outputfile.println(" termination time  " + completedProcess[i].getTerminationTime() );
-	    		            outputfile.println(" turnaround time  " + completedProcess[i].getTurnaroundTime());
-	    		            outputfile.println(" waiting time  " + completedProcess[i].getWaitingTime() );
-	    		            outputfile.println(" response time  " + completedProcess[i].getResponseTime() );
-	    		          
-	    		    			
-	    		    }
-	    		    }
+	    	    public static void writeInFile(){
+	    	    	
+		    		try {
+		    		    File f = new File("report1.txt");
+		    		    FileOutputStream f2 = new FileOutputStream(f);
+		    		    PrintWriter outputfile= new PrintWriter(f2);
+		    		    outputfile.println("process order:" +processOrder +"]");
+		    		    Arrays.sort(completedProcess, Comparator.nullsLast(Comparator.comparingInt(PCB::getNumberOfprocess)));
+		    		    for (int i = 0; i < completed; i++) {
+		    		    	if(completedProcess[i]!=null) {
+		    		    		outputfile.println(" The processID " +completedProcess[i].getProcessID()  );
+		    		            outputfile.println(" The priority : " +completedProcess[i].getPriority());
+		    		            outputfile.println(" The arrivalTime : " +completedProcess[i].getArrivalTime());
+		    		            outputfile.println(" The cpuBurstTime :" + completedProcess[i].getCpuBurstTime());
+		    		            outputfile.println(" Starting time: " + completedProcess[i].getStartTime() );
+		    		            outputfile.println(" termination time  " + completedProcess[i].getTerminationTime() );
+		    		            outputfile.println(" turnaround time  " + calculateTurnaroundTime(completedProcess[i]));
+		    		            outputfile.println(" waiting time  " + calculateWaitingTime(completedProcess[i]) );
+		    		            outputfile.println(" response time  " + calculateResponseTime(completedProcess[i]) );
+		    		            outputfile.println( "---------------------------------");
+		    		            
+		    		    			
+		    		    }
+		    		    }
+		    		    outputfile.println( "---------------------------------");
+		    		    outputfile.println(" Average  Turn around time= "+avgTurnaroundTime);
+		    		    outputfile.println(" Average  Waiting time= "+avgWaitingTime);
+		    		    outputfile.println(" Average  Response time= "+avgResponseTime);
+		    		    outputfile.println( "---------------------------------");
 
-	    		    outputfile.close();
-	    		    
-	    		} catch (FileNotFoundException ex) {
-	    			
-	    		 } catch (IOException ex) {
-	    		 
-	    		 }   
-	    		}
-	    		
-	    		public static void readFromFile(){
-	    		    try{
-	    		      File f = new File("report1.txt");
-	    		      Scanner sc = new Scanner(f);
-	    		           while(sc.hasNext()){
-	    		              System.out.println(sc.nextLine());
-	    		            }
-	    		           sc.close();
-	    		      }catch(IOException ex){
-	    		    
-	    		   }
-	    		  }
+		    		    outputfile.close();
+		    		    
+		    		} catch (FileNotFoundException ex) {
+		    			
+		    		 } catch (IOException ex) {
+		    		 
+		    		 }   
+		    		}
+		    		
+		    		public static void readFromFile(){
+		    		    try{
+		    		      File f = new File("report1.txt");
+		    		      Scanner sc = new Scanner(f);
+		    		           while(sc.hasNext()){
+		    		              System.out.println(sc.nextLine());
+		    		            }
+		    		           sc.close();
+		    		      }catch(IOException ex){
+		    		    
+		    		   }
+		    		  }
+	                        
+	      public static int calculateWaitingTime(PCB process) {
+	        return  calculateTurnaroundTime( process)- process.getCpuBurstTime();
+	     }
+
+	    public static int calculateTurnaroundTime(PCB process) {
+	     return process.getTerminationTime() - process.getArrivalTime();
+	   }
+
+	           
+
+	        
+	  
+	    public static int calculateResponseTime(PCB process) {
+	    // Check if the process has started execution
+	    if (process.getStartTime() != -1) {
+	        // Response time = Start time - Arrival time
+	        return process.getStartTime() - process.getArrivalTime();
+	    } else {
+	        // Process has not started execution yet, so response time is not applicable
+	        return -1;
+	    }
+	 }
+	   public static void calculateAverages() {
+	    int numProcesses = completed;
+	    int totalTurnaroundTime = 0;
+	    int totalWaitingTime = 0;
+	    int totalResponseTime = 0;
+
+	    for (int i = 0; i < completed; i++) {
+	        PCB process = completedProcess[i];
+	        if (process != null) {
+	            int turnaroundTime = calculateTurnaroundTime(process);
+	            int waitingTime = calculateWaitingTime(process);
+	            totalTurnaroundTime += turnaroundTime;
+	            totalWaitingTime += waitingTime;
+	            totalResponseTime += calculateResponseTime(process);
+	        }
+	    }
+
+	     avgTurnaroundTime = (double) totalTurnaroundTime / numProcesses;
+	     avgWaitingTime = (double) totalWaitingTime / numProcesses;
+	     avgResponseTime = (double) totalResponseTime / numProcesses;
+
+	   
+	}
+
+	
+
 }
--- PCB.java
+++ PCB.java
@@ -1,127 +1,143 @@
-public class PCB {
-  private String processID;
-	    private int priority;
-	    private int arrivalTime;
-	    private int cpuBurstTime;
-	    private int startTime;
-	    private int terminationTime;
-	    private int turnaroundTime;
-	    private int waitingTime;
-	    private int responseTime;
-	    private int remainingTime ;
-	  
 
-	    // Constructor
-	    public PCB(String processID, int priority, int arrivalTime, int cpuBurstTime) {
-	        this.processID = processID;
-	        this.priority = priority;
-	        this.arrivalTime = arrivalTime;
-	        this.cpuBurstTime = cpuBurstTime;
-	        remainingTime=cpuBurstTime;
-	        this.startTime = -1; // Initialize to -1 (not started)
-	        this.terminationTime = -1; // Initialize to -1 (not terminated)
-	        this.turnaroundTime = 0;
-	        this.waitingTime = 0;
-	        this.responseTime = -1; // Initialize to -1 (not responded)
-	        
-	    }
-	    // getter
-	    public String getProcessID() {
-			return processID;
-		}
+public class PCB 
+{private String processID;
+private int priority;
+private int arrivalTime;
+private int cpuBurstTime;
+private int startTime;
+private int terminationTime;
+private int turnaroundTime;
+private int waitingTime;
+private int responseTime;
+private int remainingTime ;
+private int numberOfprocess;
 
 
-		public int getPriority() {
-			return priority;
-		}
+// Constructor
+public PCB(int numberOfprocess,String processID, int priority, int arrivalTime, int cpuBurstTime) {
+    this.processID = processID;
+    this.priority = priority;
+    this.arrivalTime = arrivalTime;
+    this.cpuBurstTime = cpuBurstTime;
+    remainingTime=cpuBurstTime;
+    this.startTime = -1; // Initialize to -1 (not started)
+    this.terminationTime = -1; // Initialize to -1 (not terminated)
+    this.turnaroundTime = 0;
+    this.waitingTime = 0;
+    this.responseTime = -1; // Initialize to -1 (not responded)
+    this.numberOfprocess= numberOfprocess;
+    
+}
+// getter
+public String getProcessID() {
+	return processID;
+}
+
+
+public int getPriority() {
+	return priority;
+}
 
 
 
-		public int getArrivalTime() {
-			return arrivalTime;
-		}
+public int getArrivalTime() {
+	return arrivalTime;
+}
 
 
-		public int getCpuBurstTime() {
-			return cpuBurstTime;
-		}
-
-		
-		public int getStartTime() {
-			return startTime;
-		}
-
-		
-
-		public int getTerminationTime() {
-			return terminationTime;
-		}
+public int getCpuBurstTime() {
+	return cpuBurstTime;
+}
 
 
-		public int getTurnaroundTime() {
-			return turnaroundTime;
-		}
-
-		
-		public int getWaitingTime() {
-			return waitingTime;
-		}
+public int getStartTime() {
+	return startTime;
+}
 
 
-		public int getResponseTime() {
-			return responseTime;
-		}
-		
-		
-	//setter
-		public int getRemainingTime() {
-			return remainingTime;
-		}
-		public void setRemainingTime(int remainingTimeQ1) {
-			this.remainingTime = remainingTimeQ1;
-		}
-		public void setProcessID(String processID) {
-			this.processID = processID;
-		}
-		public void setPriority(int priority) {
-			this.priority = priority;
-		}
-		public void setArrivalTime(int arrivalTime) {
-			this.arrivalTime = arrivalTime;
-		}
-		public void setCpuBurstTime(int cpuBurstTime) {
-			this.cpuBurstTime = cpuBurstTime;
-		}
-		public void setStartTime(int startTime) {
-			this.startTime = startTime;
-		}
-		public void setTerminationTime(int terminationTime) {
-			this.terminationTime = terminationTime;
-		}
-		public void setTurnaroundTime(int turnaroundTime) {
-			this.turnaroundTime = turnaroundTime;
-		}
-		public void setWaitingTime(int waitingTime) {
-			this.waitingTime = waitingTime;
-		}
-		public void setResponseTime(int responseTime) {
-			this.responseTime = responseTime;
-		}
-		public void StatusDetail(){
-		System.out.printf(" | The processID : %-4s" ,processID  );
-		System.out.printf(" | The priority : %-4d" , priority);
-		System.out.printf(" | The arrival Time : %-4d" , arrivalTime);
-		System.out.printf(" | The cpu Burst Time :%-4d" , cpuBurstTime);
-		System.out.printf(" | Starting time: %-4d" , startTime );
-		System.out.printf(" | termination time %-4d" , terminationTime );
-		System.out.printf(" | turnaround time  %-4d" , turnaroundTime);
-		System.out.printf(" | waiting time  %-4d" , waitingTime );
-		System.out.printf(" | response time %-4d" , responseTime);
-	}
-		@Override
-		public String toString() {
-			return "PCB [processID=" + processID ;
-		}
 
-} 
-	
+public int getTerminationTime() {
+	return terminationTime;
+}
+
+
+public int getTurnaroundTime() {
+	return turnaroundTime;
+}
+
+
+public int getWaitingTime() {
+	return waitingTime;
+}
+
+
+public int getResponseTime() {
+	return responseTime;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public int getNumberOfprocess() {
+	return numberOfprocess;
+}
+public void setNumberOfprocess(int numberOfprocess) {
+	this.numberOfprocess = numberOfprocess;
+}
+public int getRemainingTime() {
+	return remainingTime;
+}
+public void setRemainingTime(int remainingTimeQ1) {
+	this.remainingTime = remainingTimeQ1;
+}
+public void setProcessID(String processID) {
+	this.processID = processID;
+}
+public void setPriority(int priority) {
+	this.priority = priority;
+}
+public void setArrivalTime(int arrivalTime) {
+	this.arrivalTime = arrivalTime;
+}
+public void setCpuBurstTime(int cpuBurstTime) {
+	this.cpuBurstTime = cpuBurstTime;
+}
+public void setStartTime(int startTime) {
+	this.startTime = startTime;
+}
+public void setTerminationTime(int terminationTime) {
+	this.terminationTime = terminationTime;
+}
+public void setTurnaroundTime(int turnaroundTime) {
+	this.turnaroundTime = turnaroundTime;
+}
+public void setWaitingTime(int waitingTime) {
+	this.waitingTime = waitingTime;
+}
+public void setResponseTime(int responseTime) {
+	this.responseTime = responseTime;
+}
+
+@Override
+public String toString() {
+	return "PCB [processID=" + processID ;
+}
+
+
+}
